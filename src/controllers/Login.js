4d4637893c25a1b0266ee3e3b929ae3956232b2a
shah1012 <daxshah1203@gmail.com>
2022-05-10 11:02:51 -0500

    Login Done

--- src/controllers/Login.js
+++ src/controllers/Login.js
@@ -1,22 +1,42 @@
-import express from "express"
+import express from "express";
+import { mongooseModel } from "../models/UserSchema.js";
 import bcrypt from "bcrypt";
-
 
 const route = express.Router();
 
-route.post('/', (req, res ) => {
-    const uEmail = req.body.email;
-    const uPass = req.body.password;
+route.post("/", (req, res) => {
+  const uEmail = req.body.email;
+  const uPass = req.body.password;
 
-    try {
-        res.json({
-            pass: uPass,
-            email: uEmail
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(400).send("Error")
+  try {
+    if (uEmail) {
+      mongooseModel.findOne(
+        {
+          email: uEmail,
+        },
+        (err, data) => {
+          if (err) res.status(400).json(err);
+
+          if (data) {
+            const isValid = bcrypt.compareSync(uPass, data.password);
+
+            if (isValid) {
+              res.json(data);
+            } else {
+              res.status(400).json("Wrong Credentials");
+            }
+          } else {
+            res.status(400).json("Wrong Credentials");
+          }
+        }
+      );
+    } else {
+      res.status(400).json("Wrong Credentials");
     }
-})
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Error");
+  }
+});
 
 export default route;
